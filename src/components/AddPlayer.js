f7d7445fc3b8ba12e0596012fe477bb7470244f0
eradoria <eradoria@gmail.com>
2023-03-28 22:19:44 -0500

    added drop down to Addplayer

--- src/components/AddPlayer.js
+++ src/components/AddPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
@@ -7,12 +7,42 @@ import Button from "@mui/material/Button";
 import "../AddPlayer.css";
 
 export default function AddPlayer() {
+  // const [id, setId] = useState();
   const [player, setPlayer] = useState("");
   const [rank, setRank] = useState();
+  const [newRank, setNewRank] = useState();
+  const [playerList, setPlayerList] = useState([]);
 
-  // const counterId = () => {
-  //   setId(id + 1);
-  // };
+  // useEffect(() => {
+  //   axios
+  //     .get("https://ttt-backend-ht7uwdj12-eradoria.vercel.app/rankings")
+  //     .then((response) => {
+  //       setPlayerList(response.data);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    async function fetchData() {
+      // Fetch data
+      const { data } = await axios.get(
+        "https://ttt-backend-ht7uwdj12-eradoria.vercel.app/rankings"
+      );
+      const results = [];
+      // Store results in the results array
+      data.forEach((value) => {
+        results.push({
+          key: value._id,
+          value: value.player,
+        });
+      });
+      // Update the options state
+      setPlayerList([{ key: "", value: "" }, ...results]);
+    }
+    // console.log(playerList);
+
+    // Trigger the fetch
+    fetchData();
+  }, []);
 
   const handleListing = async () => {
     try {
@@ -25,32 +55,20 @@ export default function AddPlayer() {
     }
   };
 
+  const handleReadded = async (id) => {
+    try {
+      axios.put("https://ttt-backend-ht7uwdj12-eradoria.vercel.app/update", {
+        id: id,
+        newRank: newRank,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
-      {" "}
       <div className="form">
-        {/* <input
-          type="text"
-          name="Player"
-          placeholder="Name"
-          onChange={(event) => {
-            setPlayer(event.target.value);
-          }}
-        />
-
-        <input
-          type="text"
-          name="Rank"
-          placeholder="Rank"
-          onChange={(event) => {
-            setRank(event.target.value);
-          }}
-        /> */}
-        {/* 
-        <button className="add-button" onClick={handleListing}>
-          Save
-        </button> */}
-
         <Box
           component="form"
           sx={{
@@ -75,13 +93,53 @@ export default function AddPlayer() {
               setRank(event.target.value);
             }}
           />
+
+          <Stack spacing={2} direction="row">
+            <Button
+              className="button"
+              variant="contained"
+              onClick={handleListing}
+            >
+              Save
+            </Button>
+          </Stack>
+        </Box>
+      </div>
+
+      <div className="form2">
+        <Box
+          component="form"
+          sx={{
+            "& > :not(style)": { m: 1, width: "25ch" },
+          }}
+          noValidate
+          autoComplete="off"
+        >
+          <select>
+            {playerList.map((x) => {
+              return (
+                <option key={x.key} value={x.id}>
+                  player {x.value}
+                </option>
+              );
+            })}
+          </select>
+
+          <TextField
+            id="standard-basic"
+            label="Rank"
+            variant="standard"
+            onChange={(event) => {
+              setNewRank(event.target.value);
+            }}
+          />
         </Box>
 
         <Stack spacing={2} direction="row">
           <Button
             className="button"
             variant="contained"
-            onClick={handleListing}
+            onClick={handleReadded}
           >
             Save
           </Button>
